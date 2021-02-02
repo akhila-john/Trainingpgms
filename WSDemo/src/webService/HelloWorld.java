2dd8b35828a6cdca807a161776a04d42b711b66c
akhila john <akhilajohn8@gmail.com>
2021-02-01 22:26:08 -0800

    commit

---
+++ WSDemo/src/webService/HelloWorld.java
@@ -0,0 +1,31 @@
+package webService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/helloworld")
+public class HelloWorld {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello World";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello World "
+				+ "</hello>";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello World"
+				+ "</title>" + "<body><h1>" + "Hello World"
+				+ "</body></h1>" + "</html> ";
+	}
+}
